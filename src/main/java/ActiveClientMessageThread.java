eebfaf18c1f112390ee724a981d4e4ca53295fb3
Shak623 <shak.farooq2@gmail.com>
2020-11-07 15:51:42 -0500

    Created active and passive threads for client messaging

---
+++ src/main/java/ActiveClientMessageThread.java
@@ -0,0 +1,13 @@
+public class ActiveClientMessageThread extends Thread{
+
+    private ClientNode node;
+
+    public ActiveClientMessageThread(ClientNode node){
+        this.node = node;
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
---
+++ src/main/java/PassiveClientMessageThread.java
@@ -0,0 +1,13 @@
+public class PassiveClientMessageThread extends Thread{
+
+    private ClientNode node;
+
+    public PassiveClientMessageThread(ClientNode node){
+        this.node = node;
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
